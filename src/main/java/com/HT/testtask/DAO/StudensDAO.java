58c96bfe222fc3a50ed797739dc0d4296ae491e6
Viktor <einevic@gmail.com>
2016-10-29 00:43:00 +0400

    update

--- src/main/java/com/HT/testtask/DAO/StudensDAO.java
+++ src/main/java/com/HT/testtask/DAO/StudensDAO.java
@@ -21,47 +21,45 @@ public class StudensDAO {
 
         initDatabase(connectionPool);
 
-        SQLContainer container = new SQLContainer(new FreeformQuery(
+        return new SQLContainer(new FreeformQuery(
                 "SELECT id, surname, name, patronymic, numGroup, date FROM StudentTable",
                 connectionPool));
-        return container;
     }
 
     private void initDatabase(JDBCConnectionPool connectionPool) {
         Connection conn = null;
         try {
             conn = connectionPool.reserveConnection();
-            Statement statement = conn.createStatement();
-            try {
+            try (Statement statement = conn.createStatement()) {
                 statement.executeUpdate("DROP TABLE StudentTable");
-            } catch (SQLException e) {
+
+                statement
+                        .execute("CREATE TABLE StudentTable "
+                                + "(id IDENTITY NOT NULL , "
+                                + "surname VARCHAR(255),"
+                                + "name VARCHAR(255),"
+                                + "patronymic VARCHAR(255), "
+                                + "numGroup INTEGER, "
+                                + "date DATE ,"
+                                + "PRIMARY KEY (id))");
+                statement
+                        .executeUpdate("INSERT INTO StudentTable "
+                                + "(surname, name, patronymic, numGroup, date) "
+                                + "VALUES ('Иванов', 'Иван', 'Иванович', '5', '1995-09-15')");
+                statement
+                        .executeUpdate("INSERT INTO StudentTable "
+                                + "(surname, name, patronymic, numGroup, date) "
+                                + "VALUES ('Васильев', 'Василий', 'Васильевич', '11', '1992-02-13')");
+                statement
+                        .executeUpdate("INSERT INTO StudentTable "
+                                + "(surname, name, patronymic, numGroup, date) "
+                                + "VALUES ('Семенов', 'Семен', 'Семенович', '7', '1998-02-01')");
+                statement
+                        .executeUpdate("INSERT INTO StudentTable "
+                                + "(surname, name, patronymic, numGroup, date) "
+                                + "VALUES ('Петров', 'Петр', 'Петрович', '2', '1995-07-20')");
+                statement.close();
             }
-            statement
-                    .execute("CREATE TABLE StudentTable "
-                            + "(id IDENTITY NOT NULL , "
-                            + "surname VARCHAR(255),"
-                            + "name VARCHAR(255),"
-                            + "patronymic VARCHAR(255), "
-                            + "numGroup INTEGER, "
-                            + "date DATE ,"
-                            + "PRIMARY KEY (id))");
-            statement
-                    .executeUpdate("INSERT INTO StudentTable "
-                            + "(surname, name, patronymic, numGroup, date) "
-                            + "VALUES ('Иванов', 'Иван', 'Иванович', '5', '1995-09-15')");
-            statement
-                    .executeUpdate("INSERT INTO StudentTable "
-                            + "(surname, name, patronymic, numGroup, date) "
-                            + "VALUES ('Васильев', 'Василий', 'Васильевич', '11', '1992-02-13')");
-            statement
-                    .executeUpdate("INSERT INTO StudentTable "
-                            + "(surname, name, patronymic, numGroup, date) "
-                            + "VALUES ('Семенов', 'Семен', 'Семенович', '7', '1998-02-01')");
-            statement
-                    .executeUpdate("INSERT INTO StudentTable "
-                            + "(surname, name, patronymic, numGroup, date) "
-                            + "VALUES ('Петров', 'Петр', 'Петрович', '2', '1995-07-20')");
-            statement.close();
             conn.commit();
         } catch (SQLException e) {
             e.printStackTrace();
