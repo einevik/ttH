b3e4ece8ee0f9683d9683fc4eba04990a5002bd4
xDeeEine <einevic@gmail.com>
2016-10-31 16:38:43 +0400

    update

--- src/main/java/com/HT/testtask/MainUI.java
+++ src/main/java/com/HT/testtask/MainUI.java
@@ -5,6 +5,7 @@ import com.HT.testtask.EditWindow;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
+import com.vaadin.data.Container;
 import com.vaadin.data.Item;
 import com.vaadin.data.Property;
 import com.vaadin.data.util.sqlcontainer.SQLContainer;
@@ -34,7 +35,10 @@ public class MainUI extends UI {
 
 
         VerticalLayout vLayout = new VerticalLayout();
+        VerticalLayout labelLayout = new VerticalLayout();
         HorizontalLayout hLayout = new HorizontalLayout();
+        HorizontalLayout findLayout = new HorizontalLayout();
+
         vLayout.setMargin(true);
 
         Table studentTable = new Table();
@@ -43,6 +47,9 @@ public class MainUI extends UI {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+        studentTable.setColumnCollapsingAllowed(true);
+        studentTable.setColumnCollapsed("ID", true);
         studentTable.setColumnHeader("SURNAME", "Фамилия");
         studentTable.setColumnHeader("NAME", "Имя");
         studentTable.setColumnHeader("PATRONYMIC", "Отчество");
@@ -50,10 +57,25 @@ public class MainUI extends UI {
         studentTable.setColumnAlignment("NUMGROUP", Table.ALIGN_CENTER);
         studentTable.setColumnHeader("DATE", "Дата рождения");
 
+        studentTable.setColumnWidth("SURNAME", 160);
+        studentTable.setColumnWidth("NAME", 160);
+        studentTable.setColumnWidth("PATRONYMIC", 160);
+        studentTable.setColumnWidth("NUMGROUP", 80);
+        studentTable.setColumnWidth("DATE", 130);
+
         studentTable.setPageLength(10);
         studentTable.setSelectable(true);
 //        studentTable.setEditable(true);
 
+        TextField findStudent = new TextField();
+        findStudent.setInputPrompt("Фамилия...");
+        findStudent.setWidth(String.valueOf(483));
+        TextField findGroup = new TextField();
+        findGroup.setInputPrompt("Группа...");
+        findGroup.setWidth(String.valueOf(83));
+
+        Label findLabel = new Label();
+//        findLabel.setValue("Фильтр");
 
         DateFormat df = new SimpleDateFormat("dd.MM.yyyy"); // ("dd MMM yyyy")
         studentTable.addGeneratedColumn("DATE", new Table.ColumnGenerator() {
@@ -67,9 +89,13 @@ public class MainUI extends UI {
         });
 
         Button add = new Button("Добавить");
+        add.setWidth(String.valueOf(232));
         Button edit = new Button("Изменить");
+        edit.setWidth(String.valueOf(232));
         Button delete = new Button("Удалить");
-
+        delete.setWidth(String.valueOf(233));
+        Button apply = new Button("Приминть");
+        apply.setWidth(String.valueOf(130));
 
         add.addClickListener(new Button.ClickListener() {
             @Override
@@ -125,6 +151,12 @@ public class MainUI extends UI {
             }
         });
 
+        vLayout.addComponent(labelLayout);
+        labelLayout.addComponent(findLabel);
+        vLayout.addComponent(findLayout);
+        findLayout.addComponent(findStudent);
+        findLayout.addComponent(findGroup);
+        findLayout.addComponent(apply);
         vLayout.addComponent(studentTable);
         vLayout.addComponent(hLayout);
         hLayout.addComponent(add);
